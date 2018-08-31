21f5583e0efb2184d51490eb3c9abd7f59b1d8f6
whusym <whusym@gmail.com>
2018-08-31 09:49:34 +0800

    added more instructions

--- src/prediction-validation.py
+++ src/prediction-validation.py
@@ -25,7 +25,7 @@ def read_predicted(path, idx, actual_dict):
         price_diff_val_total, price_diff_quant_total = 0, 0
         for row in predicted:
             row = row[:-1]   # remove the newline char at the end of each line
-            row = row.split('|')   # split the line into a list using
+            row = row.split('|')   # split the line into a list using pipe
             if int(row[0]) == idx and row[1] in actual_dict:
                 price_diff_val_total += abs(actual_dict[row[1]] - float(row[2]))
                 price_diff_quant_total += 1
@@ -53,7 +53,6 @@ def main(actual_path, predict_path, window_path, output_path):
     predict: path to the file with predicted values.
     window_path: path to the file containing the numerical window value.
     output_path: path to the file containing comparison value at each window.
-    TODO: add more instructions.
     '''
 
     # Get the total number of time stamps
@@ -71,22 +70,28 @@ def main(actual_path, predict_path, window_path, output_path):
 
 
     # get a list of differences between actual and prediction at each time stamp
+    print ('Reading the prediction file...')
     diff_list = [read_predicted(predict_path, i, read_actual(actual_path, i)) for i in range(1, last_row + 1)]
+    # manipulate the diff_list and get the desired result in a certain window
+    print ('Calculating the average error...')
     res = map(division, [reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), diff_list[i:i + window_num]) for i in range(last_row + 1 - window_num)])
+    # write results
+    print ('Writing results...')
     with open(output_path, 'w') as filewrite:
         counter = 1
         for row in res:
             filewrite.write('{}|{}|{}\n'.format(counter, str(counter + int(window_num) - 1), row))
             counter += 1
+    print ('All done! Check the output file for results.')
 
 
 if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument('-a', '--actual', dest='actual_path', help='path to input file', required=True)
-    parser.add_argument('-p', '-predict', dest='predict_path', help='path to predict file', required=True)
-    parser.add_argument('-w', '-window', dest='window_path', help='path to window file', required=True)
-    parser.add_argument('-o', '-output', dest='output_path', help='path to output file', default='./output/comparison.txt')
+    parser.add_argument('-p', '--predict', dest='predict_path', help='path to predict file', required=True)
+    parser.add_argument('-w', '--window', dest='window_path', help='path to window file', required=True)
+    parser.add_argument('-o', '--output', dest='output_path', help='path to output file', default='./output/comparison.txt')
     args = parser.parse_args()
 
     main(args.actual_path, args.predict_path, args.window_path, args.output_path)
